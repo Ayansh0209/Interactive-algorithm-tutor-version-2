453c379239d31710eb1fb01ad88926c7077566d9
“Ayansh” <ayansh0209@gmail.com>
2025-08-17 01:52:22 +0530

    Added all thr types of linekd list
    but ro be reviewed to merge it to main have to make some changes

--- backend/test.py
+++ backend/test.py
@@ -1,73 +1,179 @@
 from tracer import runCode
 import json
 code = """
-class DNode:
-    def __init__(self, value):
-        self.data = value
-        self.next_ref = None
-        self.prev_ref = None
 
-class DoublyLinkedList:
+# =============================================================================
+# 1. SINGLY LINKED LIST (Simple Linked List)
+# =============================================================================
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
     def __init__(self):
-        self.head_node = None
-
-    def append(self, val):
-        node = DNode(val)
-        if not self.head_node:
-            self.head_node = node
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
             return
-        cur = self.head_node
-        while cur.next_ref:
-            cur = cur.next_ref
-        cur.next_ref = node
-        node.prev_ref = cur
+        current = self.head
+        while current.next:
+            current = current.next
+        current.next = new_node
+    
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    def display(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        print(" -> ".join(map(str, elements)) + " -> None")
 
-    def prepend(self, val):
-        node = DNode(val)
-        if self.head_node:
-            self.head_node.prev_ref = node
-        node.next_ref = self.head_node
-        self.head_node = node
+# =============================================================================
+# 2. CIRCULAR LINKED LIST
+# =============================================================================
 
-    def insert_after(self, target_val, new_val):
-        cur = self.head_node
-        while cur and cur.data != target_val:
-            cur = cur.next_ref
-        if cur:
-            node = DNode(new_val)
-            node.next_ref = cur.next_ref
-            node.prev_ref = cur
-            if cur.next_ref:
-                cur.next_ref.prev_ref = node
-            cur.next_ref = node
+class CircularNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = CircularNode(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = new_node  # Points to itself
+            return
+        
+        # Find the last node (the one pointing to head)
+        current = self.head
+        while current.next != self.head:
+            current = current.next
+        
+        # Insert new node
+        current.next = new_node
+        new_node.next = self.head
+    
+    def prepend(self, data):
+        new_node = CircularNode(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = new_node
+            return
+        
+        # Find the last node
+        current = self.head
+        while current.next != self.head:
+            current = current.next
+        
+        # Insert at beginning
+        new_node.next = self.head
+        current.next = new_node
+        self.head = new_node
+    
+    def display(self):
+        if not self.head:
+            print("Empty circular list")
+            return
+        
+        elements = []
+        current = self.head
+        elements.append(current.data)
+        current = current.next
+        
+        # Traverse until we come back to head
+        while current != self.head:
+            elements.append(current.data)
+            current = current.next
+        
+        print(" -> ".join(map(str, elements)) + " -> (back to " + str(self.head.data) + ")")
+
+# =============================================================================
+# 3. CIRCULAR DOUBLY LINKED LIST (Bonus)
+# =============================================================================
+
+class CircularDNode:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class CircularDoublyLinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = CircularDNode(data)
+        if not self.head:
+            self.head = new_node
+            new_node.next = new_node
+            new_node.prev = new_node
+            return
+        
+        # Get the last node (head.prev)
+        last = self.head.prev
+        
+        # Insert new node
+        new_node.next = self.head
+        new_node.prev = last
+        last.next = new_node
+        self.head.prev = new_node
+    
     def display_forward(self):
-        cur = self.head_node
-        while cur:
-            print(cur.data, end=" <-> ")
-            cur = cur.next_ref
-        print("None")
+        if not self.head:
+            print("Empty circular doubly list")
+            return
+        
+        elements = []
+        current = self.head
+        elements.append(current.data)
+        current = current.next
+        
+        while current != self.head:
+            elements.append(current.data)
+            current = current.next
+        
+        print("Forward: " + " <-> ".join(map(str, elements)) + " <-> (circular)")
 
-    def display_backward(self):
-        cur = self.head_node
-        while cur and cur.next_ref:
-            cur = cur.next_ref
-        while cur:
-            print(cur.data, end=" <-> ")
-            cur = cur.prev_ref
-        print("None")
+# =============================================================================
+# TEST CODE
+# =============================================================================
 
-# Test
-dll = DoublyLinkedList()
-dll.append(10)
-dll.append(20)
-dll.append(40)
-dll.insert_after(20, 30)
-dll.prepend(5)
-dll.display_forward()
-dll.display_backward()
+print("=== SINGLY LINKED LIST ===")
+sll = LinkedList()
+sll.append(10)
+sll.append(20)
+sll.append(30)
+sll.prepend(5)
+sll.display()
 
+print("=== CIRCULAR LINKED LIST ===")
+cll = CircularLinkedList()
+cll.append(100)
+cll.append(200)
+cll.append(300)
+cll.prepend(50)
+cll.display()
 
+print("=== CIRCULAR DOUBLY LINKED LIST ===")
+cdll = CircularDoublyLinkedList()
+cdll.append(1000)
+cdll.append(2000)
+cdll.append(3000)
+cdll.display_forward()
 
 
 """
--- backend/tracer.py
+++ backend/tracer.py
@@ -6,7 +6,6 @@ import json
 
 tracer = []
 prevVars = {}
-# tracks depth of function calls (useful for recursion/backtracking)
 CodeDepth = 0
 print_buffer = io.StringIO()
 FORBIDDEN_KEYWORDS = [
@@ -14,131 +13,240 @@ FORBIDDEN_KEYWORDS = [
     "import shutil", "from os", "from sys", "socket", "threading", "multiprocessing"
 ]
 
-
 def is_code_safe(code):
     lowered = code.lower()
     return not any(kw in lowered for kw in FORBIDDEN_KEYWORDS)
 
-def nextAttribute(obj, exclude=()):
-    """Find an attribute that points to another object of the same type."""
-    for attr in dir(obj):
-        if attr.startswith("__") or attr in exclude:
-            continue
-        try:
-           val = getattr(obj, attr)
-           if isinstance(val, type(obj)) or val is None:
-                return attr
-        except:
-            continue
+def get_object_attributes(obj):
+    """Get non-method attributes of an object"""
+    attrs = {}
+    if not hasattr(obj, '__dict__'):
+        return attrs
+    
+    try:
+        for attr_name, attr_value in obj.__dict__.items():
+            if not callable(attr_value):
+                attrs[attr_name] = attr_value
+    except:
+        pass
+    return attrs
+
+def detect_linked_list_type(obj):
+    """Detect if object is a linked list container or node"""
+    if not hasattr(obj, '__dict__'):
+        return None
+    
+    attrs = get_object_attributes(obj)
+    class_name = type(obj).__name__.lower()
+    
+    # Use class name as primary indicator
+    if 'doublylinkedlist' in class_name or 'doublelinkedlist' in class_name:
+        return "doubly_linked_list"
+    elif 'linkedlist' in class_name:
+        return "linked_list"
+    elif 'dnode' in class_name or ('node' in class_name and any(attr in attrs for attr in ['prev', 'prev_ref'])):
+        return "doubly_linked_node"
+    elif 'node' in class_name:
+        return "linked_list_node"
+    
+    # Check for container patterns (has head/root reference)
+    container_indicators = ['head', 'head_node', 'root', 'first']
+    for indicator in container_indicators:
+        if indicator in attrs:
+            ref_obj = attrs[indicator]
+            if ref_obj is not None and hasattr(ref_obj, '__dict__'):
+                # Check if referenced object looks like a node
+                ref_attrs = get_object_attributes(ref_obj)
+                node_indicators = ['next', 'next_ref', 'prev', 'prev_ref', 'left', 'right', 'data', 'value']
+                if any(ind in ref_attrs for ind in node_indicators):
+                    # Determine list type based on node structure
+                    if any(ind in ref_attrs for ind in ['prev', 'prev_ref']):
+                        return "doubly_linked_list"
+                    else:
+                        return "linked_list"
+            # Empty container - check class name for hints
+            elif ref_obj is None:
+                if 'doubly' in class_name.lower():
+                    return "doubly_linked_list"
+                else:
+                    return "linked_list"
+    
+    # Check for node patterns
+    node_indicators = ['next', 'next_ref', 'prev', 'prev_ref', 'left', 'right']
+    data_indicators = ['data', 'value', 'val']
+    
+    has_node_ref = any(ind in attrs for ind in node_indicators)
+    has_data = any(ind in attrs for ind in data_indicators)
+    
+    if has_node_ref or has_data:
+        # Check if it's doubly linked
+        if any(ind in attrs for ind in ['prev', 'prev_ref']):
+            return "doubly_linked_node"
+        else:
+            return "linked_list_node"
+    
     return None
 
+def traverse_linked_structure(start_obj):
+    """Traverse and map a linked structure"""
+    if not start_obj:
+        return []
+    
+    # If it's a container, find the head
+    head_node = start_obj
+    container_attrs = get_object_attributes(start_obj)
+    
+    for head_attr in ['head', 'head_node', 'root', 'first']:
+        if head_attr in container_attrs and container_attrs[head_attr] is not None:
+            head_node = container_attrs[head_attr]
+            break
+    
+    if not head_node or not hasattr(head_node, '__dict__'):
+        return []
+    
+    # Traverse the linked structure
+    nodes = []
+    visited = set()
+    current = head_node
+    node_id = 0
+    id_to_index = {}
+    
+    # First pass: collect all nodes
+    while current and id(current) not in visited:
+        visited.add(id(current))
+        id_to_index[id(current)] = node_id
+        
+        attrs = get_object_attributes(current)
+        
+        # Find data value
+        data_value = None
+        for data_attr in ['data', 'value', 'val']:
+            if data_attr in attrs:
+                data_value = attrs[data_attr]
+                break
+        
+        # Store raw connections for now
+        raw_connections = {}
+        for ref_attr in ['next', 'next_ref', 'prev', 'prev_ref', 'left', 'right']:
+            if ref_attr in attrs and attrs[ref_attr] is not None:
+                raw_connections[ref_attr] = id(attrs[ref_attr])
+        
+        nodes.append({
+            'id': node_id,
+            'value': data_value,
+            'raw_connections': raw_connections
+        })
+        
+        node_id += 1
+        
+        # Move to next node (prefer 'next' or 'next_ref')
+        next_node = None
+        for next_attr in ['next', 'next_ref']:
+            if next_attr in attrs:
+                next_node = attrs[next_attr]
+                break
+        current = next_node
+        
+        # Safety limit
+        if len(nodes) > 50:
+            break
+    
+    # Second pass: convert object IDs to node indices
+    for node in nodes:
+        connections = {}
+        for attr, obj_id in node['raw_connections'].items():
+            if obj_id in id_to_index:
+                connections[attr] = id_to_index[obj_id]
+        node['connections'] = connections
+        del node['raw_connections']
+    
+    return nodes
+
+def create_linked_structure_json(obj, structure_type):
+    """Create JSON representation of linked structure"""
+    nodes = traverse_linked_structure(obj)
+    
+    # Normalize type name for display
+    display_type = structure_type
+    if display_type.endswith('_node'):
+        display_type = display_type[:-5]  # Remove '_node' suffix
+    
+    # Ensure all values in nodes are JSON serializable
+    safe_nodes = []
+    for node in nodes:
+        safe_node = {
+            "id": node["id"],
+            "value": safe_json_serialize(node["value"]),
+            "connections": node["connections"]  # These should be integers
+        }
+        safe_nodes.append(safe_node)
+    
+    return {
+        "type": display_type,
+        "nodes": safe_nodes,
+        "head": 0 if safe_nodes else None
+    }
+
 def detectType(val):
+    """Main type detection function"""
     try:
-        if isinstance(val, (int, float, str, bool, type(None))):
+        # Handle None and primitives
+        if val is None or isinstance(val, (int, float, str, bool)):
             return "primitive"
-        if isinstance(val, list):
+        if isinstance(val, (list, tuple)):
             return "array"
         if isinstance(val, dict):
             return "object"
         if callable(val):
             return "function"
-
-        tname = type(val).__name__.lower()
-
-        # Detect linked list container
-        if hasattr(val, "__dict__"):
-            for attr in dir(val):
-                if attr.startswith("__"):
-                    continue
-                node = getattr(val, attr, None)
-                if hasattr(node, "__dict__"):
-                    link_attr = nextAttribute(node)
-                    if link_attr:
-                        if hasattr(node, "prev") or nextAttribute(node, exclude=(link_attr,)):
-                            return "doubly_linked_list"
-                        return "linked_list"
-
-        # Detect linked list node
-        link_attr = nextAttribute(val)
-        if link_attr:
-            if hasattr(val, "prev") or nextAttribute(val, exclude=(link_attr,)):
-                return "doubly_linked_node"
-            return "linked_list_node"
-
-        return tname
+        
+        # Check for linked structures
+        linked_type = detect_linked_list_type(val)
+        if linked_type:
+            return linked_type
+        
+        # Check if object is being constructed (has no attributes yet)
+        if hasattr(val, '__dict__'):
+            attrs = get_object_attributes(val)
+            if not attrs:
+                return "constructing"
+            
+        # Default to class name
+        return type(val).__name__.lower()
+        
     except Exception:
         return "unknown"
 
-def visualFormat(container):
-    """Return JSON-safe visualization format for supported data structures."""
-    result = {"type": "linked_list", "head": None, "nodes": [], "current": None}
-    visited = {}
-    node_id_counter = 0
-
-    # Find starting node dynamically
-    start_node = None
-    for attr in dir(container):
-        if attr.startswith("__"):
-            continue
-        node = getattr(container, attr, None)
-        if hasattr(node, "__dict__") and nextAttribute(node) is not None:
-
-            start_node = node
-            result["head"] = 0
-            break
-    if not start_node:
-        return None
-
-    current = start_node
-    prev_id = None
-    while current and id(current) not in visited:
-        node_id = node_id_counter
-        visited[id(current)] = node_id
-        node_data = None
-        for attr in dir(current):
-            if attr.startswith("__"):
-                continue
-            val = getattr(current, attr, None)
-            if not hasattr(val, "__dict__"):
-                node_data = val
-                break
-
-        next_node = getattr(current, nextAttribute(current), None)
-        next_id = None
-        if next_node and id(next_node) not in visited:
-            next_id = node_id_counter + 1
-
-        result["nodes"].append({
-            "id": node_id,
-            "value": node_data,
-            "next": next_id,
-            "prev": prev_id
-        })
-
-        prev_id = node_id
-        current = next_node
-        node_id_counter += 1
-
-    return result
-
-        
-        
-   
+def ensure_json_serializable(obj):
+    """Recursively ensure all objects are JSON serializable"""
+    if obj is None or isinstance(obj, (int, float, str, bool)):
+        return obj
+    elif isinstance(obj, dict):
+        return {k: ensure_json_serializable(v) for k, v in obj.items()}
+    elif isinstance(obj, (list, tuple)):
+        return [ensure_json_serializable(item) for item in obj]
+    else:
+        # Any other type - convert to string
+        if hasattr(obj, '__dict__'):
+            return f"<{type(obj).__name__} object>"
+        else:
+            return str(obj)
+def safe_json_serialize(val):
+    """Safely serialize a value to JSON-compatible format"""
+    return ensure_json_serializable(val)
 
 def FilterCode(sourceCode):
+    """Filter executable lines from source code"""
     tree = ast.parse(sourceCode)
     exec_lines = set()
 
     def is_skipped_top_stmt(node):
-        # Skip defs/imports
         if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef, ast.Import, ast.ImportFrom)):
             return True
-        # Skip module docstring (Expr[str])
         if isinstance(node, ast.Expr) and isinstance(getattr(node, "value", None), ast.Constant) and isinstance(node.value.value, str):
             return True
         return False
 
-    # Only consider top-level statements
     for node in getattr(tree, "body", []):
         if is_skipped_top_stmt(node):
             continue
@@ -149,19 +257,16 @@ def FilterCode(sourceCode):
 
     return sorted(exec_lines)
 
-
-    
-tracingSteps =False # global flag it wont be true until we wil get value output for out visualizer
+tracingSteps = False
 MainLines = set()
     
 def traceSteps(frame, event, arg):
-    global prevVars, CodeDepth,tracingSteps
+    global prevVars, CodeDepth, tracingSteps
 
     if event not in ["call", "line", "return"]:
         return traceSteps
 
-    codeLine = linecache.getline(
-        frame.f_code.co_filename, frame.f_lineno).strip()
+    codeLine = linecache.getline(frame.f_code.co_filename, frame.f_lineno).strip()
     if not codeLine and "__code_lines__" in frame.f_globals:
         try:
             codeLine = frame.f_globals["__code_lines__"][frame.f_lineno - 1].strip()
@@ -169,76 +274,80 @@ def traceSteps(frame, event, arg):
             codeLine = ""
 
     funcName = frame.f_code.co_name
-    # Don't trace module lines before the first executable top-level statement
+    
+    # Skip lines before main execution starts
     if frame.f_code.co_name == "<module>" and frame.f_lineno < START_AT:
         return traceSteps
 
-
-    # Start tracing when we hit the first allowed executable line
+    # Only start tracing when we hit main execution lines
     if not tracingSteps:
         if frame.f_lineno not in MainLines:
             return traceSteps
+    
+    # Check if we have user variables to start tracing
     user_locals = {k: v for k, v in frame.f_locals.items() if not k.startswith("__")}
     if any(not callable(v) for v in user_locals.values()):
         tracingSteps = True
     else:
         return traceSteps
 
+    # Clean up frame locals - exclude Python internals
+    clean_locals = {k: v for k, v in frame.f_locals.items() 
+                   if not k.startswith("__") and k != "__code_lines__"}
 
-
-  
-    
-    PyNoise = frame.f_locals.copy()
-
-    # Remove built-in clutter
-    if "__builtins__" in PyNoise:
-        del PyNoise["__builtins__"]
-
-    if "__code_lines__" in PyNoise:
-        del PyNoise["__code_lines__"]
-
-   
     safe_locals = {}
     type_info = {}
-    # changed_values = {}  # NEW: store changed values
 
-    for var, val in PyNoise.items():
+    for var, val in clean_locals.items():
         if callable(val):
             continue
+            
         var_type = detectType(val)
         type_info[var] = var_type
         
+        # Handle linked structures - these get special JSON representation
         if var_type in ("linked_list", "linked_list_node", "doubly_linked_list", "doubly_linked_node"):
             try:
-                safe_locals[var] = visualFormat(val) 
-            except:
-                safe_locals[var] = repr(val) 
-            continue 
-        
-        try:
-            safe_locals[var] = json.loads(json.dumps(val))
-        except:
-            safe_locals[var] = repr(val)
-       
-        
-            
-        
-        
-     
-    # Detect loop or condition scope
-    scope_type = None
-    if codeLine.startswith("for") or codeLine.startswith("while"):
-        scope_type = "loop"
-    elif codeLine.startswith("if") or codeLine.startswith("elif") or codeLine.startswith("else"):
-        scope_type = "conditional"
-    elif event == "call":
-        scope_type = "function"
+                linked_json = create_linked_structure_json(val, var_type)
+                safe_locals[var] = linked_json
+            except Exception as e:
+                # Fallback for errors
+                display_type = var_type.replace("_node", "") if var_type.endswith("_node") else var_type
+                safe_locals[var] = {
+                    "type": display_type,
+                    "nodes": [],
+                    "head": None,
+                    "error": str(e)[:100]
+                }
+        # Handle constructing objects
+        elif var_type == "constructing":
+            safe_locals[var] = f"<{type(val).__name__} constructing...>"
+        else:
+            # Handle ALL other values through safe serialization
+            safe_locals[var] = safe_json_serialize(val)
 
-    # Try to detect if a conditional branch is taken (True/False)
+    # Detect scope type
+    scope_type = None
+    if event == "call":
+        scope_type = "function"
+    elif codeLine.startswith(("for ", "while ")):
+        scope_type = "loop"
+    elif codeLine.startswith(("if ", "elif ", "else")):
+        scope_type = "conditional"
+
+    # Branch detection for conditionals
     branch_taken = None
-    if scope_type == "conditional":
+    if scope_type == "conditional" and event == "line":
         try:
-            branch_taken = eval(codeLine, frame.f_globals, frame.f_locals)
+            # Simple heuristic - this is approximate
+            if codeLine.startswith("if "):
+                condition = codeLine[3:].rstrip(":")
+                branch_taken = eval(condition, frame.f_globals, frame.f_locals)
+            elif codeLine.startswith("elif "):
+                condition = codeLine[5:].rstrip(":")
+                branch_taken = eval(condition, frame.f_globals, frame.f_locals)
+            elif codeLine.startswith("else"):
+                branch_taken = True
         except:
             branch_taken = None
 
@@ -248,36 +357,35 @@ def traceSteps(frame, event, arg):
     elif event == "return":
         CodeDepth -= 1
 
-    # Append full trace snapshot
-    tracer.append({
+    # Create trace entry - ensure everything is JSON serializable
+    trace_entry = {
         "event": event,
         "line": frame.f_lineno,
         "function": funcName,
-        "locals": safe_locals,
-        # "code": codeLine,
-        #  "changed_vars": changedVars,
-        # "changed_values": changed_values,  # NEW: add changed_values dict
+        "locals": ensure_json_serializable(safe_locals),
         "scope": scope_type,
         "depth": CodeDepth,
         "var_types": type_info,
         "branch_taken": branch_taken
-    })
+    }
 
-    prevVars = {k: v for k, v in safe_locals.items() if k != "__code_lines__"}
+    tracer.append(trace_entry)
+    prevVars = {k: v for k, v in safe_locals.items()}
     return traceSteps
 
-
 def runCode(code: str):
-    global tracer, prevVars, CodeDepth, print_buffer,tracingSteps, MainLines,START_AT
+    global tracer, prevVars, CodeDepth, print_buffer, tracingSteps, MainLines, START_AT
 
+    # Reset global state
     tracer = []
     prevVars = {}
     CodeDepth = 0
     print_buffer = io.StringIO()
     tracingSteps = False
-    MainLines = set(FilterCode(code)) # this is tho hold lines number not def class import lines 
+    MainLines = set(FilterCode(code))
     START_AT = min(MainLines) if MainLines else 1
 
+    # Safety check
     if not is_code_safe(code):
         return [{
             "event": "exception",
@@ -285,17 +393,23 @@ def runCode(code: str):
         }]
 
     try:
+        # Prepare execution environment
         globals_dict = {"__code_lines__": code.splitlines()}
-
+        
+        # Capture stdout
         old_stdout = sys.stdout
         sys.stdout = print_buffer
 
+        # Execute with tracing
         compiled = compile(code, "<string>", "exec")
         sys.settrace(traceSteps)
         exec(compiled, globals_dict)
         sys.settrace(None)
 
+        # Restore stdout
         sys.stdout = old_stdout
+        
+        # Capture any printed output
         printed_output = print_buffer.getvalue()
         if printed_output.strip():
             tracer.append({
@@ -311,4 +425,6 @@ def runCode(code: str):
             "error": str(e)
         })
 
-    return tracer
+    # FINAL SAFETY: Ensure the entire tracer output is JSON serializable
+    safe_tracer = ensure_json_serializable(tracer)
+    return safe_tracer
